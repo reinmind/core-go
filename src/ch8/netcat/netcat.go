efabc512146f435be4d4bf87b1c1c632dfc7f28f
xiang.zhang <acjason@163.com>
2021-10-14 11:13:05 +0800

    concurrent example

--- src/ch8/netcat/netcat.go
+++ src/ch8/netcat/netcat.go
@@ -8,10 +8,32 @@ import (
 	"time"
 )
 
-var gv uint64
+type Point struct {
+	X float32
+	Y float32
+}
+
+func (p *Point) MoveXForward() {
+	p.X += 1
+}
+func (p *Point) MoveYForward() {
+	p.Y += 1
+}
+func (p *Point) MoveForward() {
+	p.X += 1
+	p.Y += 1
+}
+func (p Point) MoveBackward() {
+	p.X -= 1
+	p.Y -= 1
+}
+func (p Point) String() string {
+	return fmt.Sprintf("X:%.2f , Y:%.2f", p.X, p.Y)
+}
 
 func main() {
-	gv = 1
+
+	p := Point{X: 0, Y: 0}
 	listener, err := net.Listen("tcp", "localhost:8000")
 	if err != nil {
 		log.Fatal(err)
@@ -24,18 +46,24 @@ func main() {
 			continue
 		}
 		// handle one connection concurrently
-		go handleConn(conn)
+		go handleConn(conn, &p)
 	}
 }
 
-func handleConn(c net.Conn) {
-	x := 1
+// communicate by sharing memory causing concurrent problems
+func handleConn(c net.Conn, p *Point) {
+
 	defer c.Close()
 	for {
-		x += 1
-		gv += 1
+		p.MoveXForward()
+		time.Sleep(20 * time.Millisecond)
+		p.MoveYForward()
+		time.Sleep(20 * time.Millisecond)
+		p.MoveXForward()
+		time.Sleep(20 * time.Millisecond)
+		p.MoveYForward()
 		_, err := io.WriteString(c, time.Now().Format("15:04:05  "))
-		io.WriteString(c, fmt.Sprintf("lv:%d gv:%d\n", x, gv))
+		io.WriteString(c, fmt.Sprintf("%v\n", *p))
 		if err != nil {
 			return
 		}
